9621bef32f26b3ced6fefbb5b0ef90090a2d8327
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-03-22 00:16:59 -0300

    Add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import Koa, { Request } from 'koa';
 import Router from 'koa-router';
 import graphqlHTTP from 'koa-graphql';
+import cors from '@koa/cors';
 import { schema } from './schema';
 
 const app = new Koa();
 const router = new Router();
+
+app.use(
+  cors({
+    origin: '*',
+    allowMethods: ['GET', 'HEAD', 'PUT', 'POST', 'DELETE', 'PATCH']
+  })
+);
 
 router.all(
   '/graphql',
