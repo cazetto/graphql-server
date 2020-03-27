12938dfda56b659458e2a545ea9ac1c299c78052
André Pesci Cazetto <cazetto.andre@gmail.com>
2020-03-26 22:04:51 -0300

    Add product module

--- src/modules/RootQueryType.ts
+++ src/modules/RootQueryType.ts
@@ -3,6 +3,9 @@ import { GraphQLObjectType, GraphQLList } from 'graphql';
 import { UserType } from './user/UserType';
 import { UserModel } from './user/UserModel';
 
+import { ProductType } from './product/Type';
+import { products } from './product/Resolvers';
+
 const RootQueryType = new GraphQLObjectType({
   name: 'Query',
   description: 'Root query',
@@ -11,6 +14,12 @@ const RootQueryType = new GraphQLObjectType({
       type: new GraphQLList(UserType),
       resolve() {
         return UserModel.find({});
+      }
+    },
+    products: {
+      type: new GraphQLList(ProductType),
+      resolve() {
+        return products();
       }
     }
   }
